09b7e061adad1b7e5237180ab3d47e1f83f5247e
Arminda Estrada <armindaestrada@email.arizona.edu>
2017-02-27 16:05:44 -0700

    fun
    Added a few equations.
    P1, P2. P3 and P4 are under construction

--- InitialCode.m
+++ InitialCode.m
@@ -5,9 +5,10 @@ tau = 10;
 P_SC = 1/tau; % probability of SC dividing unevenly
 z = 14; %number of times progenitor cell divides before dying
 Ub = 10^-8; %probability of assymetric division gaining jak2 mutation
+Ua = 2*10^-6; % symetric division without altering cell's phenotype
 alpha = 2; %ratio of symmetric vs. asymmetric
-beta = 5; %additonal number of divisions
-Z_Jak2 = z +beta; %number of divisions progenitor cells with jak2 mutations will undergo before dying
+gamma = 5; %additonal number of divisions
+Z_Jak2 = z +gamma; %number of divisions progenitor cells with jak2 mutations will undergo before dying
 d = 0.05; %death rate of cell 
 %may want to update death rate after mutation 
 lineage_cell = z*N*P_SC; %number of progenitor cell at each time step
@@ -29,8 +30,8 @@ prog_death = N*P_SC*(2*exp(z)); % number of progenitor cells that will die
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %%%%%Scenario (i). A Stem Cell Acquires JAK2V617F.%%%%%
-p1(0) = 0;  %probability of Scenerio 1
-dP1 = (1-P1)*(N/tau)*(1+d)*(1/2+alpha)*ub;
+P1 = 0;  %probability of Scenerio 1
+dP1 = (1-P1)*(N/tau)*(1+d)*(1/2+alpha)*Ub;
 %N/tau: number of stem cell divides
 %dN/tau: expected # of births stem cells undergo per time step to compensate for cell death
 % (1/2 + alpha)*ub: probablilty that mutation arises
@@ -41,10 +42,12 @@ dP1 = (1-P1)*(N/tau)*(1+d)*(1/2+alpha)*ub;
 
 %%%%%Scenario (ii). A Progenitor Acquires a Mutation Conferring
 %%%%%Self-Renewal Followed by JAK2V617F%%%
-p2 = 1- exp(-(N/tau)*sum(Fk*Gk*(1-d),1,t-z)); 
- Fk = exp(-(2^(z - 1)*ua*(K-1)))*((2^z)-1)*ua;
- Gk = 1- exp(-((2^z -1)*ub + exp(-((2^z) -1)*ub)*(2^z)*ub*(t-z-k)));
+% p2 = 1- exp(-(N/tau)*sum(Fk*Gk*(1-d),1,t-z)); 
+%  Fk = exp(-(2^(z - 1)*ua*(K-1)))*((2^z)-1)*ua;   %expected number of cells in which a mutation conferring self-renewal emerged at time k,
+%  Gk = 1- exp(-((2^z -1)*ub + exp(-((2^z) -1)*ub)*(2^z)*ub*(t-z-k))); %probability that the JAK2V617F mutation emerges in the clone carrying the first mutation between times k and t.
  
+ 
+ p2 = (N/2)*P_SC*2^(2*z)*Ua*Ub*((t-z)^2)*(1+d);
  
  
 
@@ -52,6 +55,7 @@ p2 = 1- exp(-(N/tau)*sum(Fk*Gk*(1-d),1,t-z));
 %%%%%Scenario (iii). A Progenitor Acquires JAK2V617F Followed by a Mutation
 %%%%%Conferring Self-Renewal.%%%%
 
+%p3 = N*P_SC*(2^(z+gamma-1))*(z+1)*Ua*Ub((t - (z/2+gamma))*(1+d));
 
 
 
@@ -59,6 +63,23 @@ p2 = 1- exp(-(N/tau)*sum(Fk*Gk*(1-d),1,t-z));
 %%%%%Scenario (iv). A Stem Cell Acquires a Mutation Conferring Self-Renewal
 %%%%%to a Progenitor, Followed by the JAK2V617F Mutation Arising in a
 %%%%%Progenitor.%%%%%%%%
+
+
+%Lk = (fk*Ua/2 + (1-fk)); %expected number of progenitors at the most undifferentiated stage carrying the self-renewal mutation
+%p4 = 1 - exp(-(N+P_SC)*sum(Lk*Gk)*(1-d));
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
